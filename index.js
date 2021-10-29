46c0f4cee4ec9de46a7436b986a4ea59d5caf27e
dev-sweet <sweetali0520@gmail.com>
2021-10-30 02:13:37 +0600

    project setup with node express mongo

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+// uri and mongo client
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mas8d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("transcrew");
+    const serviceCollection = database.collection("services");
+
+    // get all users bye GET Method
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+    // client.close();
+  }
+}
+run().catch(console.dir());
+app.get("/", (req, res) => {
+  res.send("Transcrew server is running");
+  console.log("transcrew server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port : http://localhost:${port}`);
+});
